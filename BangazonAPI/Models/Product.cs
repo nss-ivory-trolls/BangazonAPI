5f31a66c5f5a4d5ccc9216b8196381c8cf527341
colebryant <cobryant47@gmail.com>
2019-04-01 09:29:01 -0500

    added Computer, Computer Employee, Product, and ProductType to Models

--- BangazonAPI/Models/Product.cs
+++ BangazonAPI/Models/Product.cs
@@ -7,5 +7,22 @@ namespace BangazonAPI.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+        public string Title { get; set; }
+
+        public int Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
     }
 }
