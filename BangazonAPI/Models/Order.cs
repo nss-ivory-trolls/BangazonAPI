f6156b191de2afcd52df5e9bd43e1d4f6882849a
Ashwin Prakash <ashp94@gmail.com>
2019-04-01 09:29:35 -0500

    added classes for our database

---
+++ BangazonAPI/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer customer { get; set; }
+
+        public int PaymentTypeId { get; set; }
+
+        public PaymentType paymenttype { get; set; }
+    }
+}
